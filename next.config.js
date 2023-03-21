4c2cbd0144e05326b70003a8e6581aafc3ad1302
Rafael Henrique Carneiro <rafael.h@outlook.com>
2023-03-21 19:28:29 -0300

    Configurando ambiente Next.js

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  swcMinify: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'coronado-pet.vercel.app',
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
